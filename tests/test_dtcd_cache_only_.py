b84a8502814bba5eeab2d3cddf98132802bb055f
yangshuang70 <yangshuang68@jd.com>
2022-08-08 10:53:36 +0800

    tests: insert.

--- tests/test_dtcd_cache_only_.py
+++ tests/test_dtcd_cache_only_.py
@@ -1,6 +1,50 @@
 import pytest
 import pymysql
 
+def test_insert_with_single_quotes():
+    db = pymysql.connect(host='127.0.0.1', port=20015, user='test', password='test', database='test')
+    cursor = db.cursor()
+    sql = "insert into opensource(uid, name) values(1, 'hello') where uid = 1"
+    cursor.execute(sql)
+    db.commit()
+    rowsaffected = cursor.rowcount
+    cursor.close()
+    db.close()
+    assert rowsaffected == 1
+
+'''
+def test_insert_with_double_quotes():
+    db = pymysql.connect(host='127.0.0.1', port=20015, user='test', password='test', database='test')
+    cursor = db.cursor()
+    sql = "insert into opensource(uid, name) values(1, \"hello\") where uid = 1"
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+'''    
+
+'''
+def test_insert_remove_where_cluster():
+    db = pymysql.connect(host='127.0.0.1', port=20015, user='test', password='test', database='test')
+    cursor = db.cursor()
+    sql = "insert into opensource(uid, name) values(1, \"hello\")"
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+'''
+
+'''
+def test_insert_remove_where_cluster_without_specify_key():
+    db = pymysql.connect(host='127.0.0.1', port=20015, user='test', password='test', database='test')
+    cursor = db.cursor()
+    sql = "insert into opensource values(1, \"Jack\", \"Shanghai\", 1, 18)"
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+'''
+
 def test_select():
     db = pymysql.connect(host='127.0.0.1', port=20015, user='test', password='test', database='test')
     cursor = db.cursor()
