a4dfcc9e39669d7048082ae7d97f0b317552a53d
yangshuang70 <yangshuang68@jd.com>
2022-08-10 16:41:30 +0800

    docker: update

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 sleep_count=0
-echo "111111111111"
-while [ $sleep_count -le 10 ]
+
+while [ $sleep_count -le 100 ]
 do
     if [ -f "/etc/dtc/dtc.yaml" ]; then 
         echo "Start running process."
         /usr/local/dtc/dtcd -d
     else
         echo "sleeping"
-        sleep 10s
+        sleep 1s
         let sleep_count+=1
     fi
 done
