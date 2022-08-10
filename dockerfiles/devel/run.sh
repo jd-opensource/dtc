7750973ee9542f7275989db746044804f96b61bf
yangshuang70 <yangshuang68@jd.com>
2022-08-10 09:29:02 +0800

    docker: update

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -1,8 +1,12 @@
 confpath=/etc/dtc
 
+echo $INPUT_GITHUB
 if [$INPUT_GITHUB != ""]; then 
+    echo "1111111111"
     cp $INPUT_GITHUB/dtc.yaml $confpath/dtc.yaml
     cp $INPUT_GITHUB/log4cplus.conf $confpath/log4cplus.conf
 fi
+
+echo "22222222"
 
 /usr/local/dtc/dtcd -d
