f8c40225e657f7e772f137dd70c0943245fe5e14
yangshuang70 <yangshuang68@jd.com>
2022-08-10 23:33:46 +0800

    docker: update

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -5,17 +5,8 @@ sleep_count=0
 while [ $sleep_count -le 100 ]
 do
     if [ -f "/etc/dtc/dtc.yaml" ]; then 
-        echo "Start running process."$1
-        if [[ $1 == "dtc" ]]; then
-            echo "entry dtc"
-            /usr/local/dtc/dtcd -d
-        elif [[ $1 == "agent" ]]; then
-            echo "entry agent"
-            /usr/local/dtc/agent
-        else
-            echo "exiting..."$1
-            exit 0
-        fi
+        echo "Start running process."$DTC_BIN","$DTC_ARGV
+        /usr/local/dtc/$DTC_BIN $DTC_ARGV
     else
         echo "sleeping"$sleep_count
         sleep 1s
