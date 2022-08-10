ddaee26c223690df8cfe71cc13ae445e40a7809a
yangshuang70 <yangshuang68@jd.com>
2022-08-10 17:26:20 +0800

    tests: scence 2.

--- tests/init.s2.sql
+++ tests/init.s2.sql
@@ -1,4 +1,76 @@
-CREATE TABLE `opensource` (
+CREATE TABLE `opensource_0` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_1` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_2` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_3` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_4` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_5` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_6` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_7` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_8` (
+  `uid` int(11) ,
+  `name` varchar(50),
+  `city` varchar(50),
+  `sex` int(11) ,
+  `age` int(11)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `opensource_9` (
   `uid` int(11) ,
   `name` varchar(50),
   `city` varchar(50),
