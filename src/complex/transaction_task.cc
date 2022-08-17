4e0c85625c6c59ab36ba1aa28a20313f9eecfdca
yangshuang70 <yangshuang68@jd.com>
2022-08-17 22:42:03 +0800

    async-connector: update.

--- src/complex/transaction_task.cc
+++ src/complex/transaction_task.cc
@@ -592,9 +592,11 @@ CBufferChain *encode_show_db_row_data(MysqlConn* dbconn, CBufferChain *bc, uint8
 	return nbc;
 }
 
-CBufferChain *encode_show_tables_dtc(CBufferChain *bc, uint8_t &pkt_nr, std::string dbname)
+CBufferChain *encode_show_tables_dtc(CTaskRequest *request, std::string dbname)
 {
-	CBufferChain *nbc = bc;
+	uint8_t pkt_nr = request->get_seq_num();
+	pkt_nr++;
+
 	//calc current row len
 	int row_len = 0;
 	YAML::Node dtc = g_config.get_conf();
@@ -607,6 +609,7 @@ CBufferChain *encode_show_tables_dtc(CBufferChain *bc, uint8_t &pkt_nr, std::str
 	//alloc new buffer to store row data.
 	int packet_len = sizeof(CBufferChain) +
 				sizeof(MYSQL_HEADER_SIZE) + row_len;
+
 	CBufferChain *nbuff = (CBufferChain *)MALLOC(packet_len);
 	if (nbuff == NULL) {
 		return NULL;
@@ -616,7 +619,7 @@ CBufferChain *encode_show_tables_dtc(CBufferChain *bc, uint8_t &pkt_nr, std::str
 	nbuff->nextBuffer = NULL;
 
 	char *r = nbuff->data;
-	encode_mysql_header(nbuff, row_len, pkt_nr++);
+	encode_mysql_header(nbuff, row_len, pkt_nr);
 	int offset = 0;
 	offset += sizeof(MYSQL_HEADER_SIZE);
 
@@ -626,10 +629,7 @@ CBufferChain *encode_show_tables_dtc(CBufferChain *bc, uint8_t &pkt_nr, std::str
 	memcpy(r + offset, tbtypestr.c_str(), tbtypestr.length());
 	offset += tbtypestr.length();
 
-	nbc->nextBuffer = nbuff;
-	nbc = nbc->nextBuffer;
-
-	return nbc;
+	return nbuff;
 }
 
 CBufferChain *encode_show_tables_row_data(MysqlConn* dbconn, CBufferChain *bc, uint8_t &pkt_nr, std::string dbname)
@@ -956,7 +956,7 @@ int TransactionTask::request_db_query(std::string request_sql, CTaskRequest *req
 			CBufferChain* rba = NULL;
 			if(request->cmd == QUERY_CMD_SHOW_TABLES)
 			{
-				rba = encode_mysql_ok(request, 0); //encode_show_tables_dtc(pos, pkt_nr, request->get_dbname());
+				rba = encode_mysql_ok(request, 0); //encode_show_tables_dtc(request, request->get_dbname());
 				if(rba)
 					request->set_buffer_chain(rba);
 				return 0;
