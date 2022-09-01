0b672d319cbd25e01e3e142bd8bf48ea7529c1cd
yangshuang70 <yangshuang68@jd.com>
2022-09-01 15:45:15 +0800

    docker: update

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -6,10 +6,6 @@ while [ $sleep_count -le 100 ]
 do
     if [ -f "/usr/local/dtc/conf/dtc.yaml" ]; then 
 
-        cd /usr/local/lib/
-        ln -snf libdtc.so.1  libdtc.so
-        ln -snf libdtcapi.so  libdtc.so.1
-
         echo "Start running process: "$DTC_BIN","$DTC_ARGV
         cd /usr/local/dtc/bin/
         ./$DTC_BIN $DTC_ARGV
@@ -22,3 +18,7 @@ do
 done
 sleep 1000000s
 echo "Timeout waitting for dtc conf files."
+
+#cd /usr/local/lib/
+#ln -snf libdtc.so.1  libdtc.so
+#ln -snf libdtcapi.so  libdtc.so.1
