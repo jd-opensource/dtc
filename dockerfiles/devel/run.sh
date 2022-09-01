6b8f07782a0ce5c0c9b5022430331ca2dd8855fd
yangshuang70 <yangshuang68@jd.com>
2022-09-01 11:41:03 +0800

    docker: update

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -5,6 +5,11 @@ sleep_count=0
 while [ $sleep_count -le 100 ]
 do
     if [ -f "/usr/local/dtc/conf/dtc.yaml" ]; then 
+
+        cd /usr/local/lib/
+        ln -snf libdtc.so.1  libdtc.so
+        ln -snf libdtcapi.so  libdtc.so.1
+
         echo "Start running process: "$DTC_BIN","$DTC_ARGV
         cd /usr/local/dtc/bin/
         ./$DTC_BIN $DTC_ARGV
