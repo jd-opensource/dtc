fdc77ae8a46e3e95becf0c8463d38f0036c650e8
Yang Shuang <yangshuang68@jd.com>
2022-09-01 17:30:24 +0800

    fix: ubuntu version rollback to 20.04 (#100)
    * docker: ubuntu version rollback to 20.04

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -5,6 +5,8 @@ sleep_count=0
 while [ $sleep_count -le 100 ]
 do
     if [ -f "/usr/local/dtc/conf/dtc.yaml" ]; then 
+        mkdir /etc/dtc
+        cp /usr/local/dtc/conf/log4cplus.conf /etc/dtc
         echo "Start running process: "$DTC_BIN","$DTC_ARGV
         cd /usr/local/dtc/bin/
         ./$DTC_BIN $DTC_ARGV
@@ -15,5 +17,8 @@ do
         let sleep_count+=1
     fi
 done
-
 echo "Timeout waitting for dtc conf files."
+
+#cd /usr/local/lib/
+#ln -snf libdtc.so.1  libdtc.so
+#ln -snf libdtcapi.so  libdtc.so.1
