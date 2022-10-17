0b27a4fc7040dc8048bdb868a3657d643d82f8c8
chenyujie28 <chenyujie28@jd.com>
2022-10-17 11:17:27 +0800

    fix_sql_parser_fetch_address

--- script/download.sh
+++ script/download.sh
@@ -1,1 +1,1 @@
-wget https://github.com/DTC8/sql-parser/releases/download/hsql.plus-daefd23/hsql.plus-daefd23.tar.gz -O hsql.tar.gz
+wget https://github.com/DTC8/sql-parser/releases/download/hsql.plus-eda6288/hsql.plus-eda6288.tar.gz -O hsql.tar.gz
