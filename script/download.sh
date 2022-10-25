3693fb288861505a23ea73a02faa1319a34a5e63
Yang Shuang <yangshuang68@jd.com>
2022-10-25 18:46:27 +0800

    hsql updated, supporting INSERT INTO SET (#206)
    * hsql.plus-0ca1fb6
    
    * download hsql.

--- script/download.sh
+++ script/download.sh
@@ -1,1 +1,1 @@
-wget https://github.com/DTC8/sql-parser/releases/download/hsql.plus-eda6288/hsql.plus-eda6288.tar.gz -O hsql.tar.gz
+wget https://github.com/DTC8/sql-parser/releases/download/hsql.plus-0ca1fb6/hsql.plus-0ca1fb6.tar.gz -O hsql.tar.gz
