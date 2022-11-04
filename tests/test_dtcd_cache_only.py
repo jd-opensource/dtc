41792d6df85b2cb89eec8176bfce8a05431da969
yangshuang70 <yangshuang68@jd.com>
2022-11-04 21:14:55 +0800

    update hsql version and test cases.

--- tests/test_dtcd_cache_only.py
+++ tests/test_dtcd_cache_only.py
@@ -49,13 +49,13 @@ def test_update():
     rowsaffected = cursor.execute(sql)
     assert rowsaffected == 1
 
-    #sql = "update opensource set name = \"Lee3\" where uid = 1"
-    #rowsaffected = cursor.execute(sql)
-    #assert rowsaffected == 1
+    sql = "update opensource set name = \"Lee3\" where uid = 1"
+    rowsaffected = cursor.execute(sql)
+    assert rowsaffected == 1
 
-    #sql = "update opensource set name = `Lee` where uid = 1"
-    #rowsaffected = cursor.execute(sql)
-    #assert rowsaffected == 0
+    sql = "update opensource set name = `Lee` where uid = 1"
+    rowsaffected = cursor.execute(sql)
+    assert rowsaffected == 0
 
     #sql = "update opensource set name = \"Lee4\" where uid = '1'"
     #rowsaffected = cursor.execute(sql)
@@ -98,7 +98,7 @@ def test_update():
         name = row[1]
         print("uid=%s, name=%s" % (uid, name))
         assert uid == 1
-        assert name == "Lee"
+        assert name == "Lee3"
     cursor.close()
 
     db.close()
