bff8ee61e04dfe97cb5afb5f037d68bcea006b97
root <root@SHJD-LT-002177.360buyAD.local>
2022-11-08 15:54:00 +0800

    fix_test_py

--- tests/test_dtcd_cache_only.py
+++ tests/test_dtcd_cache_only.py
@@ -57,9 +57,9 @@ def test_update():
     rowsaffected = cursor.execute(sql)
     assert rowsaffected == 1
 
-    # sql = "update opensource set \"name\" = 'Lee' where uid = 1"
-    # rowsaffected = cursor.execute(sql)
-    # assert rowsaffected == 0
+    sql = "update opensource set \"name\" = 'Lee' where uid = 1"
+    rowsaffected = cursor.execute(sql)
+    assert rowsaffected == 0
 
     sql = "update opensource set name = \"Lee4\" where uid = '1'"
     rowsaffected = cursor.execute(sql)
@@ -69,7 +69,7 @@ def test_update():
     rowsaffected = cursor.execute(sql)
     assert rowsaffected == 0        
    
-    sql = "update opensource set name = 'Lee' where uid = 1"
+    sql = "update opensource set name = \"Lee\" where uid = 1"
     rowsaffected = cursor.execute(sql)
     assert rowsaffected == 1    
 
